1de4e4870f8d6bbf0916d4bf04b57387fc7c431e
a-bhandary <aryanbhandary1@gmail.com>
2026-06-28 17:29:41 +0300

    Add files via upload

---
+++ First_program.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+// This is a single line comment
+
+/*This
+is
+a
+multi-line
+comment
+*/
+
+
+int main() {
+    printf("I like Pizza\n");
+    printf("It tastes good");
+
+    return 0;
+}
